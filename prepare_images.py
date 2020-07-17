55f8a9c52bfa223193ba8de9ae52f0201fe370a3
atoka <makrusan@gmail.com>
2020-07-17 23:09:05 +0300

    Change images type to RGBA

--- prepare_images.py
+++ prepare_images.py
@@ -1,18 +1,16 @@
 import os
 from PIL import Image
 
-d = 'res'
-for filename in os.listdir(d):
-    src = os.path.join(d, filename)
-    im = Image.open(src)
+for f in os.listdir('download'):
+    im = Image.open(os.path.join('download', f))
     width, height = im.size
+    
+    filename = os.path.splitext(f[49:])[0]
+    
+    ldst = os.path.join('res', filename + '_l.png')
+    im.crop((0, 0, width/2, height)).convert('RGBA').save(ldst)
 
-    ldst = os.path.join(d, os.path.splitext(filename)[0] + '_l' + '.png')
-    lim = im.crop((0, 0, width/2, height))
-    lim.save(ldst)
-
-    rdst = os.path.join(d, os.path.splitext(filename)[0] + '_r' + '.png')
-    rim = im.crop((width/2, 0, width, height))
-    rim.save(rdst)
+    rdst = os.path.join('res', filename + '_r.png')
+    im.crop((width/2, 0, width, height)).convert('RGBA').save(rdst)
 
 
