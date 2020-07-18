9e8042fd561e4f3743aec525f087fc937809048f
atoka <makrusan@gmail.com>
2020-07-18 06:08:34 +0300

    Add OpenGL initialization

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
+#include <GL/glew.h>
+
 #include <GLFW/glfw3.h>
-#include <thread>
 
 int main() {
     glfwInit();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    const auto *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+    GLFWwindow *window = glfwCreateWindow(mode->width, mode->height, "OpenGL", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
     glfwTerminate();
 }
